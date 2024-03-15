8d165c0d2162fbcca5c53c9ff35cce6c0ae2ffc9
El Mehdi Lafkaihi <115099306+ElMehdiiiii@users.noreply.github.com>
2024-03-15 18:46:26 +0000

    Create inputcell.h

---
+++ inputcell.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "constdef.h"
+#include <QLineEdit>
+
+class CInputCell : public QLineEdit
+{
+	Q_OBJECT
+
+public:
+	CInputCell(QWidget *parent);
+	~CInputCell();
+
+	void InitCell();
+
+	void SetPos(int nRow, int nCol);
+	void GetPos(int& nRow, int& nCol);
+
+	void SetClueMode(const QString& strText);
+	void SetViewMode(const QString& strText);
+	void SetInputMode(const QString& strText);
+
+    void SetClueModeError(const QString &strText);
+protected:
+
+	void keyPressEvent(QKeyEvent * event);
+
+	void focusInEvent(QFocusEvent * event);
+	void focusOutEvent(QFocusEvent * event);
+
+protected Q_SLOTS:
+
+	void  SlotTextChanged(const QString &text);
+
+Q_SIGNALS:
+
+	void SignalSubmitData(int nRow,int nCol, const QString& strText ,CInputCell* pCell);
+	
+private:
+
+	int m_nRow;
+	int m_nCol;
+
+	QString m_strCurrentText;
+
+};
