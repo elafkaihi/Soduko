44db7810013b1fde07ab577c2d665946578f1de1
El Mehdi Lafkaihi <115099306+ElMehdiiiii@users.noreply.github.com>
2024-03-15 18:45:07 +0000

    Create extrawnd.h

---
+++ extrawnd.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "constdef.h"
+#include <QWidget>
+
+class CExtraWnd : public QWidget
+{
+	Q_OBJECT
+
+public:
+	CExtraWnd(QWidget *parent);
+    ~CExtraWnd();
+
+	enum 
+	{
+		en_Margin = 10,
+		en_CellSize = 45,
+		en_CellUnit = 9,
+		en_WndHeight = en_CellSize * en_CellUnit + 2 * en_Margin,
+	};
+
+protected:
+
+	void InitWnd();
+
+	void resizeEvent(QResizeEvent *event);
+
+	void DrawClueData(QPainter& painter, int& nVal,QRect& rect);
+	void DrawViewData(QPainter& painter, int& nVal, QRect& rect);
+	
+
+private:
+
+	QRect m_rcAvailable;
+	SudokuData::Sudoku m_sudoku;
+
+	QPen m_cluePen;
+	QBrush m_clueBrush;
+
+	QPen m_viewPen;
+	QBrush m_viewBrush;
+
+};
